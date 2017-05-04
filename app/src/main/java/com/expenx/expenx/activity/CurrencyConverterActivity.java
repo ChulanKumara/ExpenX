c9626d2f577fc4f977a67eac3b5d250a9def4920
skaveesh <skaveesh@gmail.com>
2017-05-04 22:46:06 +0530

    Calculator implemented

--- app/src/main/java/com/expenx/expenx/activity/CurrencyConverterActivity.java
+++ app/src/main/java/com/expenx/expenx/activity/CurrencyConverterActivity.java
@@ -1,15 +1,36 @@
 package com.expenx.expenx.activity;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.expenx.expenx.R;
+import com.expenx.expenx.core.CalculatorDialog;
 
 public class CurrencyConverterActivity extends AppCompatActivity {
+
+    //this is just an example. delete these when you are actually implementing
+    public Button mOpenCalculator;
+    public EditText mFinalCalculatorValueEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currency_converter);
+
+        mFinalCalculatorValueEditText = (EditText) findViewById(R.id.editTextCalculatorFinalBalue) ;
+
+        mOpenCalculator = (Button) findViewById(R.id.buttonCalculorOpen);
+        mOpenCalculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CalculatorDialog calculatorDialog = new CalculatorDialog();
+                calculatorDialog.showDialog(CurrencyConverterActivity.this, mFinalCalculatorValueEditText);
+            }
+        });
     }
 }
